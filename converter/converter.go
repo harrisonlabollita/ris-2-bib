94f0289489c42a7dc3e1cc05d6556945433d548b
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2022-08-12 19:25:40 -0700

    cleaned up converter

--- converter/converter.go
+++ converter/converter.go
@@ -23,38 +23,42 @@ type bibentry struct {
     doi string
 }
 
-func create_bib_entry(content []string) bibentry {
-    var bib bibentry = bibentry{}
+
+func bib_map(bib *bibentry, key string , val string){
+    if key == "AU" {
+        bib.authors = append(bib.authors, val)
+    } else if key == "TI" {
+        bib.title = val
+    } else if key == "JO" {
+        bib.journal = val
+    } else if key == "UR" {
+        bib.url = val
+    } else if key == "DO" {
+        bib.doi = val
+    } else if key == "VL" {
+        bib.volume = val
+    } else if key == "ID" {
+        bib.id = val
+    } else if key == "TY" {
+        bib.bibtype = val
+    } else if key == "PY" {
+        bib.year = val
+    } else if key == "SP" {
+        bib.startpg = val
+    } else if key == "EP" {
+        bib.endpg = val
+    } else if key == "IS" {
+        bib.issue = val
+    }
+}
+
+func create_bib_entry(content []string) *bibentry {
+    bib := &bibentry{}
     for i := 0; i < len(content); i++ {
         l := strings.Split(content[i], " - ")
-        if len(l) > 1 { // this is a valid entry
-            var key string = strings.TrimSpace(l[0])
-            var val string = strings.TrimSpace(l[1])
-            if key == "AU" {
-                bib.authors = append(bib.authors, val)
-            } else if key == "TI" {
-                bib.title = val
-            } else if key == "JO" {
-                bib.journal = val
-            } else if key == "UR" {
-                bib.url = val
-            } else if key == "DO" {
-                bib.doi = val
-            } else if key == "VL" {
-                bib.volume = val
-            } else if key == "ID" {
-                bib.id = val
-            } else if key == "TY" {
-                bib.bibtype = val
-            } else if key == "PY" {
-                bib.year = val
-            } else if key == "SP" {
-                bib.startpg = val
-            } else if key == "EP" {
-                bib.endpg = val
-            } else if key == "IS" {
-                bib.issue = val
-            }
+        if len(l) > 1 {     // this is a valid entry
+            key,val := strings.TrimSpace(l[0]), strings.TrimSpace(l[1])
+            bib_map(bib,key,val)
         }
     }
     return bib
